af6c31354e45ea5d291f428f99f5be8a0e020f54
Dr Nripanka Das <nripankadas@gmail.com>
2026-04-11 09:50:51 +0400

    Add src/colortool/__init__.py

---
+++ src/colortool/__init__.py
@@ -0,0 +1,7 @@
+"""colortool - Convert and manipulate colors across HEX/RGB/HSL formats."""
+
+from .core import Color, ColorError
+
+__version__ = "0.1.0"
+__author__ = "Nripanka Das"
+__all__ = ["Color", "ColorError"]
